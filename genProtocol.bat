4e0c1d968f70ebe2496641c18e12142b64bb5a3f
Zhang Kun <kun.zhang@ubisoft.com>
2021-03-10 16:12:41 +0800

    refactor

--- genProtocol.bat
+++ genProtocol.bat
@@ -1,4 +1,3 @@
 pushd protocol
-..\tools\ProtocolGen.exe -p "TypeDefine.xml"			-b Buildin_Types.xml.cfg -s go -l utf-8 -m packet
-..\tools\ProtocolGen.exe -p "TypeDefineCactus.xml"	    -b Buildin_Types.xml.cfg -s go -l utf-8 -m packet
+..\tools\ProtocolGen.exe -p TypeDefineCactus.xml -b ..\tools\Buildin_Types.xml.cfg -s go -l utf-8 -m packet
 popd
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/zhksoftGo/Packet
+module github.com/ZhkSoftGo/Packet
 
 go 1.15
